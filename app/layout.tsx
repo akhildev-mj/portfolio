141d97fca84c912b316beed245208426742a4416
akhildev-mj <akhildev.tech@gmail.com>
2025-05-18 08:24:53 +0530

    Refactor: Updated styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${jakarta.variable} ${inter.variable} font-sans`}
         suppressHydrationWarning
